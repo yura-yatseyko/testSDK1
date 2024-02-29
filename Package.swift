23ad2025490eb465e98b0d25c77d4ba78e060488
Yuriy Yatseyko <yura.yatseyko@volpis.com>
2024-02-29 11:39:33 +0200

    First commit

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hmlongco/Factory", from: "2.3.1"),
-        .package(name: "MyLibrary1", path: "./MyLibrary1")
+        .package(name: "MyLibrary1", path: "MyLibrary1")
 //        .package(path: "./MyLibrary1")
 //        .package(url: "https://github.com/yura-yatseyko/-MyLibrary1", branch: "main")
     ],
@@ -27,7 +27,7 @@ let package = Package(
             name: "PurpleSDK",
             dependencies: [
                 .product(name: "Factory", package: "Factory"),
-                .product(name: "MyLibrary1", package: "MyLibrary1")
+                "MyLibrary1"
 //                .product(name: "MyLibrary1", package: "-MyLibrary1")
             ]
         ),
